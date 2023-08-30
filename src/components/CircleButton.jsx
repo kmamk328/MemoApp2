d2a6de14dc3a38bfda83fdf12fe18dd0dd645773
daisuke koma <pinponpan328@gmail.com>
2023-08-30 22:26:21 +0900

    app

--- src/components/CircleButton.jsx
+++ src/components/CircleButton.jsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     right: 40,
     bottom: 40,
     shadowColor: '#000000',
-    shadowOffset: { width: 0, height: 8},
+    shadowOffset: { width: 0, height: 8 },
     shadowOpacity: 0.25,
     shadowRadius: 8,
   },
