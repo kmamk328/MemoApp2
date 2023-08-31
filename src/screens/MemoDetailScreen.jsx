e524fd834f37eb5af42f78a24b632ca97096d9e9
daisuke koma <pinponpan328@gmail.com>
2023-08-31 11:44:18 +0900

    Create MemoDetailScreen

---
+++ src/screens/MemoDetailScreen.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { View , ScrollView, Text, StyleSheet } from 'react-native';
+
+import AppBar from "../components/AppBar";
+import CircleButton from "../components/CircleButton";
+
+export default function MemoDetailScreen() {
+ return (
+  <View style={styles.container}>
+    <AppBar />
+    <View style={styles.memoHeader}>
+      <Text style={styles.memoTitle}>かいものりすと</Text>
+      <Text style={styles.memoDate}>2023年 8月 31日 10:26</Text>
+    </View>
+    <ScrollView style={styles.memoBody}>
+      <Text style={styles.memoText}>
+      reactでjsxって何？
+      JSXは、Reactライブラリで使用されるJavaScriptの拡張構文です。
+      JSXは、ReactコンポーネントのUIを記述するために使用されます。
+      </Text>
+    </ScrollView>
+    <CircleButton style={{ top:160, bottom: 'auto' }}>+</CircleButton>
+  </View>
+ );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  memoHeader: {
+    backgroundColor: '#467FD3',
+    height: 96,
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 19,
+  },
+  memoTitle: {
+    color: '#ffffff',
+    fontSize: 20,
+    lineHeight: 32,
+    fontWeight: 'bold',
+  },
+  memoDate: {
+    color: '#ffffff',
+    fontSize: 12,
+    lineheight: 16,
+  },
+  memoBody: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+  memoText: {
+    fontSize: 16,
+    lineheight: 24,
+  }
+});
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import AppBar from '../components/AppBar';
 import MemoList from '../components/MemoList';
