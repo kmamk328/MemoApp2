5c7e6f3173f87b2d00ee5fd7049b9e4dcbc1266e
daisuke koma <pinponpan328@gmail.com>
2023-09-17 09:39:05 +0900

    create Loading component

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -5,11 +5,14 @@ import {
 import firebase from 'firebase';
 
 import Button from '../components/Button';
+import Loading from '../components/Loading';
+import { isLastDayOfMonth } from 'date-fns';
 
 export default function LoginScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
@@ -18,6 +21,8 @@ export default function LoginScreen(props) {
           index: 0,
           routes: [{ name: 'MemoList' }],
         });
+      } else {
+        setLoading(false);
       }
     });
     return unsubscribe;
@@ -35,11 +40,15 @@ export default function LoginScreen(props) {
       })
       .catch((error) => {
         Alert.alert(error.code);
+      })
+      .then(() => {
+        setLoading(false);
       });
   }
 
   return (
     <View style={styles.container}>
+      <Loading isLoading={isLoading} />
       <View style={styles.inner}>
         <Text style={styles.title}>LOG IN</Text>
         <TextInput
